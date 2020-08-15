eab60f153bcf8f1fcba7158f27205a9d2c1083bf
Amol T <amolthind@gmail.com>
2020-08-15 12:00:29 -0700

    Add API

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_bootstrap import Bootstrap
+from data import fetch_nutrition_score
+import openfoodfacts    
 app = Flask(__name__)
 
 Bootstrap(app)
@@ -12,6 +14,14 @@ def homepage():
 def another_page():
     return render_template('another_page.html')
 
+@app.route('/data')
+def data():
+    nutrition_grades = {}
+    products = ['coke', 'chips', 'apple', 'banana', 'almonds', 'chicken']
+    for product in products:
+        grade = fetch_nutrition_score(product)
+        nutrition_grades[product] = grade
+    return jsonify(nutrition_grades)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,10 @@
+import openfoodfacts
+
+def fetch_nutrition_score(item):
+    try:
+        search_result = openfoodfacts.products.search(item)
+        return search_result['products'][0]['nutrition_grades']
+    
+    
+    except: 
+        return('No grade found')
